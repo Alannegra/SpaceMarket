06addef427d9fc8490af67e498e48c7290a7cadc
Alannegra <alanrodriguez@elpuig.xeill.net>
2022-02-25 18:22:14 +0100

    scrollview profe mesure2

---
+++ app/src/main/java/com/example/navegation/SignIn.java
@@ -0,0 +1,52 @@
+package com.example.navegation;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class SignIn extends Fragment {
+    ImageView imageView;
+    NavController navController;
+    Button button;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+        button = view.findViewById(R.id.loginbutton);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_signIn_to_bottom1Fragment);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+    }
+}
